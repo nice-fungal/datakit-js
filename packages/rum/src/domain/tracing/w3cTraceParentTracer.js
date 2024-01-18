26a11032ab2093d8a6bc2be2c1639494b1893132
hbrls <shuaizhexu@gmail.com>
2024-01-18 10:47:07 +0800

    -> rum 3.0.26

--- packages/rum/src/domain/tracing/w3cTraceParentTracer.js
+++ packages/rum/src/domain/tracing/w3cTraceParentTracer.js
@@ -1,5 +1,16 @@
 import { TraceIdentifier, getCrypto } from './traceIdentifier'
 import { assign } from '@cloudcare/browser-core'
+
+// === Generate a random 64-bit number in fixed-length hex format
+function randomTraceId() {
+  var digits = '0123456789abcdef'
+  var n = ''
+  for (var i = 0; i < 16; i += 1) {
+    var rand = Math.floor(Math.random() * 16)
+    n += digits[rand]
+  }
+  return n
+}
 /**
  *
  * @param {*} traceSampled
@@ -21,20 +32,38 @@ W3cTraceParentTracer.prototype = {
       : this._spanId.toPaddedHexadecimalString()
   },
   getTraceId: function () {
-    return this.isHexTraceId
-      ? this._traceId.toDecimalString()
-      : '0000000000000000' + this._traceId.toPaddedHexadecimalString()
+    if (this.isHexTraceId) {
+      // 转化为二进制之后上报
+      return this._traceId.toDecimalString()
+    } else {
+      return (
+        this._traceId.toPaddedHexadecimalString() +
+        this._spanId.toPaddedHexadecimalString()
+      )
+    }
   },
   getTraceParent: function () {
     // '{version}-{traceId}-{spanId}-{sampleDecision}'
-    return (
-      '00-0000000000000000' +
-      this._traceId.toPaddedHexadecimalString() +
-      '-' +
-      this._spanId.toPaddedHexadecimalString() +
-      '-' +
-      (this._traceSampled ? '01' : '00')
-    )
+    if (this.isHexTraceId) {
+      // 短64位，前面补0
+      return (
+        '00-0000000000000000' +
+        this._traceId.toPaddedHexadecimalString() +
+        '-' +
+        this._spanId.toPaddedHexadecimalString() +
+        '-' +
+        (this._traceSampled ? '01' : '00')
+      )
+    } else {
+      return (
+        '00-' +
+        this.getTraceId() +
+        '-' +
+        this.getSpanId() +
+        '-' +
+        (this._traceSampled ? '01' : '00')
+      )
+    }
   },
   makeTracingHeaders: function () {
     var baseHeaders = {
