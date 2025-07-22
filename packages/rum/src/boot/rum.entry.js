b03c3367ca8e111d4c738953491da33ed9c4466b
hbrls <shuaizhexu@gmail.com>
2025-07-22 11:46:00 +0800

    [nice] rm ReplayRecord

--- packages/rum/src/boot/rum.entry.js
+++ packages/rum/src/boot/rum.entry.js
@@ -1,11 +1,11 @@
 // import { defineGlobal, getGlobalObject } from '@cloudcare/browser-core'
 import { startRum } from './rum'
 import { makeRumPublicApi } from './rumPublicApi'
-import { startRecording } from './startRecording'
-import { makeRecorderApi } from './recorderApi'
+// import { startRecording } from './startRecording'
+// import { makeRecorderApi } from './recorderApi'
 import { createDeflateEncoder, startDeflateWorker } from '../domain/deflate'
-var recorderApi = makeRecorderApi(startRecording)
-export var datafluxRum = makeRumPublicApi(startRum, recorderApi, {
+// var recorderApi = makeRecorderApi(startRecording)
+export var datafluxRum = makeRumPublicApi(startRum, 'recorderApi', {
   startDeflateWorker: startDeflateWorker,
   createDeflateEncoder: createDeflateEncoder
 })
